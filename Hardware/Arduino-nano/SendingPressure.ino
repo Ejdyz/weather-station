3385bdda603196e7c0b83bd4453896b910690fee
Jan Adam <103042103+Ejdyz@users.noreply.github.com>
2024-01-22 17:17:26 +0100

    feat: sending pressure script final

--- Hardware/Arduino-nano/SendingPressure.ino
+++ Hardware/Arduino-nano/SendingPressure.ino
@@ -1,27 +1,30 @@
-#include <BMP280.h>
-BMP280 bmp280;
+// připojení potřebných knihoven
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+// nastavení adresy senzoru
+#define BMP280_ADRESA (0x76)
+// inicializace senzoru BMP z knihovny
+Adafruit_BMP280 bmp;
 
-void setup()
-{
-  //delay in miliseconds
-  int userDelay = 20;
-  
-  // set up serial port
+// konstanta s korekcí měření v hPa
+int korekce = 32;
+
+void setup() {
+  // komunikace po sériové lince rychlostí 9600 baud
   Serial.begin(9600);
-  delay(10);
-
-  //Join I2C bus
-  Wire.begin(); 
-  bmp280.begin();
+  // zahájení komunikace se senzorem BMP280,
+  // v případě chyby je vypsána hláška po sériové lince
+  // a zastaven program
+  if (!bmp.begin(BMP280_ADRESA)) {
+    Serial.println("BMP280 senzor nenalezen, zkontrolujte zapojeni!");
+    while (1);
+  }
 }
 
-void loop()
-{
-  //Get pressure value
-  uint32_t pressure = bmp280.getPressure();
-
-  //Print the results
-  Serial.println(pressure);
-  
-  delay(userDelay);
+void loop() {
+  // načtení naměřeného tlaku ze senzoru
+  float tlak = (bmp.readPressure()/100.00) + korekce;
+  Serial.println(tlak);
+  delay(2000);
 }

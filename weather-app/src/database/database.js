fa8cfc1bf98fc5fab98fd7327c46e0948e36feeb
Ejdy <adamhonzikcz@seznam.cz>
2024-01-25 16:33:04 +0100

    feat: created connection to the database

---
+++ weather-app/src/database/database.js
@@ -0,0 +1,19 @@
+// Example for MySQL
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection error:', err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+module.exports = db;

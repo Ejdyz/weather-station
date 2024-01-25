168e1b540e4c7af22e4c2f78c33167ff32dff503
Ejdy <adamhonzikcz@seznam.cz>
2024-01-25 20:21:09 +0100

    feat: created page for showcasing icons

---
+++ weather-app/src/app/icons/page.js
@@ -0,0 +1,88 @@
+import {
+  BarometerIcon,
+  CelsiusIcon,
+  CleanNightIcon,
+  ClearDayIcon,
+  CloudyIcon,
+  FullMoonIcon,
+  HumidityIcon,
+  MoonFirstQuarterIcon,
+  MoonLastQuarterIcon,
+  MoonriseIcon,
+  MoonsetIcon,
+  MoonWaningCrescentIcon,
+  MoonWaningGibbousIcon,
+  MoonWaxingCrescentIcon,
+  MoonWaxingGibbousIcon,
+  NewMoonIcon,
+  NotAvailableIcon,
+  PartlyCloudyDayDrizzleIcon,
+  PartlyCloudyDayRainIcon,
+  PartlyCloudyDaySnowIcon,
+  PartlyCloudyIcon,
+  PartlyCloudyNightDrizzleIcon,
+  PartlyCloudyNightIcon,
+  PartlyCloudyNightRainIcon,
+  PartlyCloudyNightSnowIcon,
+  RaindropsIcon,
+  RainingIcon,
+  SnowingIcon,
+  SunriseIcon,
+  SunsetIcon,
+  ThermometerIcon
+} from "@/components/icons/Icons";
+import {CloseIcon} from "next/dist/client/components/react-dev-overlay/internal/icons/CloseIcon";
+
+const Page = () => {
+  return (
+    <>
+      <div className="flex h-32 bg-white">
+        <SunriseIcon/>
+        <SunsetIcon/>
+        <MoonriseIcon/>
+        <MoonsetIcon/>
+      </div>
+      <div className="flex h-32 bg-white">
+        <NewMoonIcon/>
+        <FullMoonIcon/>
+        <MoonFirstQuarterIcon/>
+        <MoonLastQuarterIcon/>
+        <MoonWaningCrescentIcon/>
+        <MoonWaxingCrescentIcon/>
+        <MoonWaningGibbousIcon/>
+        <MoonWaxingGibbousIcon/>
+      </div>
+      <div className="flex h-32 bg-white">
+        <ClearDayIcon/>
+        <CleanNightIcon/>
+      </div>
+      <div className="flex h-32 bg-white">
+        <RaindropsIcon/>
+        <ThermometerIcon/>
+        <BarometerIcon/>
+        <CelsiusIcon/>
+        <HumidityIcon/>
+        <NotAvailableIcon/>
+      </div>
+      <div className="flex h-32 bg-white">
+        <CloudyIcon/>
+        <RainingIcon/>
+        <SnowingIcon/>
+      </div>
+      <div className="flex h-32 bg-white">
+        <PartlyCloudyIcon/>
+        <PartlyCloudyDayDrizzleIcon/>
+        <PartlyCloudyDayRainIcon/>
+        <PartlyCloudyDaySnowIcon/>
+      </div>
+      <div className="flex h-32 bg-white">
+        <PartlyCloudyNightIcon/>
+        <PartlyCloudyNightDrizzleIcon/>
+        <PartlyCloudyNightRainIcon/>
+        <PartlyCloudyNightSnowIcon/>
+      </div>
+    </>
+  );
+};
+
+export default Page;

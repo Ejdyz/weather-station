c725c8fd64c207444979e395fb8135bdd0ce7186
Ejdy <adamhonzikcz@seznam.cz>
2024-01-25 16:32:50 +0100

    feat: created database

---
+++ weather-app/sql.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `ejdy_cz`.`weather-last-days` (`id` INT NOT NULL AUTO_INCREMENT , `day` DATETIME NOT NULL , `highestTemperatrue` FLOAT NOT NULL , `lowestTemperatrue` FLOAT NOT NULL , `highestHumidity` FLOAT NOT NULL , `lowestHumidity` FLOAT NOT NULL , `wasRaining` BOOLEAN NOT NULL , `highestRaining` FLOAT NOT NULL , `highestLight` INT NOT NULL , `averagePressure` FLOAT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `ejdy_cz`.`weather-records` (`id` INT NOT NULL AUTO_INCREMENT , `numberInDay` TINYINT NOT NULL , `time` DATETIME NOT NULL , `temperature` FLOAT NOT NULL , `humidity` FLOAT NOT NULL , `rain` FLOAT NOT NULL , `isRaining` BOOLEAN NOT NULL , `light` INT NOT NULL , `pressure` FLOAT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+
+INSERT INTO `weather-records` (`id`, `numberInDay`, `time`, `temperature`, `humidity`, `rain`, `isRaining`, `light`, `pressure`) VALUES (NULL, '1', '2024-01-25 16:30:57', '19.7', '56.2', '67.1', '1', '22090', '1018.7');

a5dd94c9f896f11ec74d096fafeb25f250ed4b8e
Ejdy <adamhonzikcz@seznam.cz>
2024-01-26 16:45:06 +0100

    feat: created APi routes

--- weather-app/src/app/api/weather/route.js
+++ weather-app/src/app/api/weather/route.js
@@ -1,34 +1,101 @@
 import { NextResponse } from 'next/server'
-import db from '../../database/database'; // Import the database connection
-
+import db from '@/database/database';
+import {env} from 'process'
 export async function POST(request) {
-  //TODO | create function that calls the database and return last record
-  console.log("TEST 1")
   //appcepting req
   const res = await request.json()
+
   //converting req to vars
-  const { temperature, humidity, pressure,sunlight,rain,isRaining,password,time,reqNumber, ...otherData} = res;
+  const {
+    temperature,
+    humidity,
+    pressure,
+    sunlight,
+    rain,
+    isRaining,
+    password,
+    time,
+    reqNumber, ...otherData
+  } = res;
 
-  console.log(res)
+  //checking password to api
+  if(password !== env.API_PASSWORD){
+    return NextResponse.json({error:"wrong password"})
+  }
+  //check every value in req if it is correct
+  if (typeof temperature !== "number") {
+    return NextResponse.json({error:"wrong temperature value"}, { status: 400 })
+  }
+  if (typeof humidity !== "number") {
+    return NextResponse.json({error:"wrong humidity value"}, { status: 400 })
+  }
+  if (typeof pressure !== "number") {
+    return NextResponse.json({error:"wrong pressure value"}, { status: 400 })
+  }
+  if (typeof sunlight !== "number") {
+    return NextResponse.json({error:"wrong sunlight value"}, { status: 400 })
+  }
+  if (typeof rain !== "number") {
+    return NextResponse.json({error:"wrong rain value"}, { status: 400 })
+  }
+  if (typeof isRaining !== "boolean") {
+    return NextResponse.json({error:"wrong isRaining value"}, { status: 400 })
+  }
+  if (typeof time !== "string") {
+    return NextResponse.json({error:"wrong time value"}, { status: 400 })
+  }
+  if (typeof reqNumber !== "number") {
+    return NextResponse.json({error:"wrong reqNumber value"}, { status: 400 })
+  }
 
+  //save req to database
+  const table = env.DB_TABLE_RECORDS.toString()
+  const sql = "INSERT INTO " + "`"+ table + "`"+ " ( `numberInDay`, `time`, `temperature`, `humidity`, `rain`, `isRaining`, `light`, `pressure`) VALUES (?,?,?,?,?,?,?,?);"
+  const values = [ reqNumber, time,temperature,humidity,rain,isRaining,sunlight,pressure]
 
+  //writing to database
+  db.query(sql, values, (err) => {
+    if (err) {
+      console.error('Database insertion error:', err);
+    } else {
+      console.log("data saved succesfully")
+    }
+  });
+
+  //checking if reqNumber is the latest number in day
+  if (reqNumber === 143){
+    //querry to mySql inserting data from last 143 records
+    const RecordTable = env.DB_TABLE_RECORDS.toString()
+    const DayTable = env.DB_TABLE_DAYS.toString()
+    const sql = "INSERT INTO " + "`"+ DayTable + "`"+ " ( day, highestTemperatrue, lowestTemperatrue, highestHumidity, lowestHumidity, wasRaining, highestRaining, highestLight, averagePressure ) SELECT NOW(), MAX(temperature) AS highestTemperatrue, MIN(temperature) AS lowestTemperatrue, MAX(humidity) AS highestHumidity, MIN(humidity) AS lowestHumidity, CASE WHEN SUM(CASE WHEN isRaining = TRUE THEN 1 ELSE 0 END) > 0 THEN TRUE ELSE FALSE END AS wasRaining, MAX(rain) AS highestRaining, MAX(light) AS highestLight, AVG(pressure) AS averagePressure FROM " + "`"+ RecordTable + "`"+ " ORDER BY id DESC LIMIT 143 ;"
+    let dbResults = []
+    db.query(sql, (err) => {
+      if (err) {
+        console.error('Error executing the query:', err);
+      }else{
+        console.log("data saved succesfully to days database")
+      }
+    });
+  }
+
+  //sending response
+  return NextResponse.json({message:"data saved succesfully"}, { status: 200 })
 }
+
 export async function GET() {
-  //TODO | create function that calls the database and return last record
-  return NextResponse.json([
-    { name: "day 0" , Humidity:1273,Temperature:2758,Pressure:1685},
-  ])
-}
+  const table = env.DB_TABLE_RECORDS.toString();
+  const sql = "SELECT * FROM " + "`"+ table + "`"+ " ORDER BY id DESC LIMIT 1";
 
-// request
-// {
-//   "temperature": 25,
-//   "humidity": 60,
-//   "pressure": 60,
-//   "windSpeed": 15,
-//   "sunlight": 12,
-//   "rain":"true",
-//   "password":"dsadasd"
-//   "time": "nejaky timestamp",
-//   "reqNumber": 12
-// }
+  let lastRecord = await new Promise((resolve, reject) => {
+    db.query(sql, (err, results) => {
+      if (err) {
+        console.error('Error executing the query:', err);
+        reject(err);
+      } else {
+        resolve(results[0]);
+      }
+    });
+  });
+
+  return NextResponse.json(lastRecord, { status: 200 })
+}

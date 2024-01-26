85e2999b1bcab4619e2fd2d46fd4651efb113c2c
Ejdy <adamhonzikcz@seznam.cz>
2024-01-26 08:38:08 +0100

    feat: updated to responsive navbar

--- weather-app/src/components/navbar/MyNavbar.js
+++ weather-app/src/components/navbar/MyNavbar.js
@@ -1,28 +1,50 @@
+"use client"
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuToggle} from "@nextui-org/navbar";
 import {Link} from "@nextui-org/link"
 import {LogoIcon} from "@/components/icons/Icons";
+import {Button} from "@nextui-org/button";
 
 export default function App({page}) {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
   return (
-    <Navbar position="static">
-      <NavbarBrand >
+  <Navbar onMenuOpenChange={setIsMenuOpen} position="static">
+    <NavbarContent>
+      <NavbarBrand>
         <LogoIcon className="w-10"/>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem isActive={page==="about"}>
-          <Link color="foreground" href="about">
-            About
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive={page==="home"}>
-          <Link color="foreground"  href="/" >
-            Home
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent className="hidden sm:flex gap-4" justify="end">
-      </NavbarContent>
-    </Navbar>
+    </NavbarContent>
+    <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarItem isActive={page==="about"}>
+        <Link color="foreground" href="about">
+          About
+        </Link>
+      </NavbarItem>
+      <NavbarItem isActive={page==="home"}>
+        <Link color="foreground"  href="/" >
+          Home
+        </Link>
+      </NavbarItem>
+    </NavbarContent>
+    <NavbarContent justify="end">
+      <NavbarMenuToggle
+        aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        className="sm:hidden"
+      />
+    </NavbarContent>
+    <NavbarMenu>
+      <NavbarItem isActive={page==="about"}>
+        <Link color="foreground" href="about">
+          About
+        </Link>
+      </NavbarItem>
+      <NavbarItem isActive={page==="home"}>
+        <Link color="foreground"  href="/" >
+          Home
+        </Link>
+      </NavbarItem>
+    </NavbarMenu>
+  </Navbar>
   );
 }

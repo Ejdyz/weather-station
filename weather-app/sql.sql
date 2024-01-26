59b7bcf5d70b77be336f838b814201f0ba97e14c
Ejdy <adamhonzikcz@seznam.cz>
2024-01-26 16:47:56 +0100

    feat: created sql file

--- weather-app/sql.sql
+++ weather-app/sql.sql
@@ -3,3 +3,30 @@ CREATE TABLE `ejdy_cz`.`weather-records` (`id` INT NOT NULL AUTO_INCREMENT , `nu
 
 
 INSERT INTO `weather-records` (`id`, `numberInDay`, `time`, `temperature`, `humidity`, `rain`, `isRaining`, `light`, `pressure`) VALUES (NULL, '1', '2024-01-25 16:30:57', '19.7', '56.2', '67.1', '1', '22090', '1018.7');
+
+
+INSERT INTO `ejdy_cz`.`weather-last-days` (
+    day,
+    highestTemperatrue,
+    lowestTemperatrue,
+    highestHumidity,
+    lowestHumidity,
+    wasRaining,
+    highestRaining,
+    highestLight,
+    averagePressure
+)
+SELECT
+	NOW(),
+    MAX(temperature) AS highestTemperatrue,
+    MIN(temperature) AS lowestTemperatrue,
+    MAX(humidity) AS highestHumidity,
+    MIN(humidity) AS lowestHumidity,
+    CASE WHEN SUM(CASE WHEN isRaining = TRUE THEN 1 ELSE 0 END) > 0 THEN TRUE ELSE FALSE END AS wasRaining,
+    MAX(rain) AS highestRaining,
+    MAX(light) AS highestLight,
+    AVG(pressure) AS averagePressure
+FROM
+    `ejdy_cz`.`weather-records`
+ORDER BY id DESC LIMIT 143;
+

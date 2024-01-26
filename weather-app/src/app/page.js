21bd121fdb6646dd1f90ad1b696f0b31f4bb7d15
Ejdy <adamhonzikcz@seznam.cz>
2024-01-26 21:00:16 +0100

    feat: view of all backgrounds

--- weather-app/src/app/page.js
+++ weather-app/src/app/page.js
@@ -1,16 +1,54 @@
 import MyNavbar from "@/components/navbar/MyNavbar";
-import Icon, {CloudyIcon} from "@/components/icons/Icons";
+import {
+  CleanNightIcon,
+  ClearDayIcon,
+  CloudyIcon, PartlyCloudyDayDrizzleIcon,
+  PartlyCloudyIcon,
+  RainingIcon,
+  SnowingIcon
+} from "@/components/icons/Icons";
 
 
-export default function Home() {
-  const clearWeather = "from-gray-300 via-cyan-500 to-blue-500"
-  const cloudyWeather = "from-gray-100 to-blue-700"
+export default async function Home() {
+  const clearWeatherDay = "from-gray-300 via-cyan-500 to-blue-600"
+  const partlyCloudyDay = "from-sky-400 to-gray-400"
+
+  const clearWeatherNight = "from-gray-700 via-gray-800 to-gray-900"
+
+  const cloudyWeather = "from-sky-300 to-gray-500"
+
+  const drizzleWeather = "from-sky-700 to-gray-400"
+  const rainyWeather = "from-sky-800  to-gray-500"
+  const snowyWeather = "from-sky-300  to-cyan-600"
+
+
   return (
-    <div className={"flex min-h-screen flex-col items-center justify-between bg-gradient-to-tr " + clearWeather}>
-      <MyNavbar page="home"/>
-      <div>
+    <>
 
+      <div className={"h-screen w-full bg-gradient-to-tr " + clearWeatherDay} >
+      <MyNavbar page="home"/>
+        <div className="w-full h-full flex items-start justify-end">
+          <ClearDayIcon className={"w-64"} />
+        </div>
       </div>
-    </div>
+      <div className={"h-screen w-full flex flex-row items-start justify-end bg-gradient-to-tr "+partlyCloudyDay} >
+        <PartlyCloudyIcon className={"w-64"} />
+      </div>
+      <div className={"h-screen w-full flex flex-row items-start justify-end bg-gradient-to-tr " + clearWeatherNight} >
+        <CleanNightIcon className={"w-64"}/>
+      </div>
+      <div className={"h-screen w-full flex flex-row items-start justify-end bg-gradient-to-tr " +cloudyWeather} >
+        <CloudyIcon className={"w-64"}/>
+      </div>
+      <div className={"h-screen w-full flex flex-row items-start justify-end bg-gradient-to-tr "+ drizzleWeather} >
+        <PartlyCloudyDayDrizzleIcon className={"w-64"}/>
+      </div>
+      <div className={"h-screen w-full flex flex-row items-start justify-end bg-gradient-to-tr " +rainyWeather } >
+        <RainingIcon className={"w-64"}/>
+      </div>
+      <div className={"h-screen w-full flex flex-row items-start justify-end bg-gradient-to-tr " + snowyWeather} >
+        <SnowingIcon className={"w-64"}/>
+      </div>
+    </>
   )
 }

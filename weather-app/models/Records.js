66163df6a1adf07ab938dc722c7ea9ee0e756a10
Ejdy <adamhonzikcz@seznam.cz>
2024-01-29 19:20:31 +0100

    feat: created db table schemas

---
+++ weather-app/models/Records.js
@@ -0,0 +1,61 @@
+/**
+ * Import Sequelize.
+ */
+const { Sequelize, DataTypes } = require("sequelize");
+
+/**
+ * Import the Sequelize instance that you have exported
+ * in the config/database.js file.
+ */
+const sequelize = require("@/database/database");
+
+/**
+ * Define a model that can be managed by Sequelize.
+ */
+const Records = sequelize.define(
+  "weather-records",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    numberInDay: {
+      type: DataTypes.TINYINT,
+      unsigned: true,
+      allowNull: false,
+    },
+    time: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    temperature: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    humidity: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    rain: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    isRaining: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    pressure: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    light: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {}
+);
+
+module.exports = Records;

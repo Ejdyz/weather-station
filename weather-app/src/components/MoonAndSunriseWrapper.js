0cda3645e14cd9121ff0da181ca4b01eb1b52085
Ejdy <adamhonzikcz@seznam.cz>
2024-01-29 10:00:16 +0100

    feat: implemented component for showing current zodiac sign

--- weather-app/src/components/MoonAndSunriseWrapper.js
+++ weather-app/src/components/MoonAndSunriseWrapper.js
@@ -2,12 +2,13 @@ import {getSunriseAndSunset} from "@/lib/weatherData";
 import Sunrise from "@/components/sunrise/Sunrise";
 import Moonrise from "@/components/moonrise/Moonrise";
 import MoonPhase from "@/components/moonphase/MoonPhase";
+import ZodiacSigns from "@/components/zodiac/ZodiacSigns";
 
 const MoonAndSunriseWrapper = async () => {
   const data = await getSunriseAndSunset()
   return (
     <div className="flex md:flex-row flex-col w-full justify-evenly mb-12 ">
-
+      <ZodiacSigns />
       <Sunrise sunrise={data.sunrise} sunset={data.sunset}/>
       <Moonrise moonrise={data.moonrise} moonset={data.moonset}/>
       <MoonPhase />

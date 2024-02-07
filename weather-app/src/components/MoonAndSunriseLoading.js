fbdfcedceb59d751e33ff941d0d260feaf1d783e
Ejdy <adamhonzikcz@seznam.cz>
2024-02-07 19:01:43 +0100

    fix: height sunrise loading

--- weather-app/src/components/MoonAndSunriseLoading.js
+++ weather-app/src/components/MoonAndSunriseLoading.js
@@ -2,7 +2,7 @@ import {Spinner} from "@nextui-org/spinner";
 
 const MoonAndSunriseLoading = () => {
   return (
-    <div className="h-[288px]  mt-8 mb-12 grid align-middle ">
+    <div className="md:h-[112px] h-[288px]   mt-8 mb-12 grid align-middle ">
       <Spinner/>
     </div>
   );

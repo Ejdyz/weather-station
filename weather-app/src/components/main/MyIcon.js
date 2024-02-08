16de9f14f648ee2d3ec0b1a878ff7df05adfaa78
Ejdy <adamhonzikcz@seznam.cz>
2024-02-08 17:03:22 +0100

    feat: margin on main icon

--- weather-app/src/components/main/MyIcon.js
+++ weather-app/src/components/main/MyIcon.js
@@ -17,22 +17,22 @@ import {
 const MyIcon = async ({cloudiness ,rain,night}) => {
 
   if (cloudiness === "cloudy") {
-    if (rain === "drizzle") return (<DrizzleIcon className={"w-64"}/>)
-    if (rain === "rain") return (<RainingIcon className={"w-64"}/>)
-    if (rain === "snow") return (<SnowingIcon className={"w-64"}/>)
-    return (<CloudyIcon className={"w-64"}/>)
+    if (rain === "drizzle") return (<DrizzleIcon className={"w-64 md:mr-28"}/>)
+    if (rain === "rain") return (<RainingIcon className={"w-64 md:mr-28"}/>)
+    if (rain === "snow") return (<SnowingIcon className={"w-64 md:mr-28"}/>)
+    return (<CloudyIcon className={"w-64 md:mr-28"}/>)
   } else {
     if (night) {
-      if (rain === "drizzle") return (<PartlyCloudyNightDrizzleIcon className={"w-64"}/>)
-      if (rain === "rain") return (<PartlyCloudyNightRainIcon className={"w-64"}/>)
-      if (rain === "snow") return (<PartlyCloudyNightSnowIcon className={"w-64"}/>)
-      return (<CleanNightIcon className={"w-64"}/>)
+      if (rain === "drizzle") return (<PartlyCloudyNightDrizzleIcon className={"w-64 md:mr-28"}/>)
+      if (rain === "rain") return (<PartlyCloudyNightRainIcon className={"w-64 md:mr-28"}/>)
+      if (rain === "snow") return (<PartlyCloudyNightSnowIcon className={"w-64 md:mr-28"}/>)
+      return (<CleanNightIcon className={"w-64 md:mr-28"}/>)
     }
-    if (cloudiness === "partly cloudy") return (<PartlyCloudyDayIcon className={"w-64"}/>)
-    if (rain === "drizzle") return (<PartlyCloudyDayDrizzleIcon className={"w-64"}/>)
-    if (rain === "rain") return (<PartlyCloudyDayRainIcon className={"w-64"}/>)
-    if (rain === "snow") return (<PartlyCloudyDaySnowIcon className={"w-64"}/>)
-    return (<ClearDayIcon className={"w-64"}/>)
+    if (cloudiness === "partly cloudy") return (<PartlyCloudyDayIcon className={"w-64 md:mr-28"}/>)
+    if (rain === "drizzle") return (<PartlyCloudyDayDrizzleIcon className={"w-64 md:mr-28"}/>)
+    if (rain === "rain") return (<PartlyCloudyDayRainIcon className={"w-64 md:mr-28"}/>)
+    if (rain === "snow") return (<PartlyCloudyDaySnowIcon className={"w-64 md:mr-28"}/>)
+    return (<ClearDayIcon className={"w-64 md:mr-28"}/>)
   }
 }
 

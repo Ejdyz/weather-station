5d999ead3c76e43d0756e6f1d9aaefcec1980b34
Ejdy <adamhonzikcz@seznam.cz>
2024-02-09 08:48:56 +0100

    fix: smaller opacity on radar

--- weather-app/src/components/map/WeatherMap.js
+++ weather-app/src/components/map/WeatherMap.js
@@ -18,7 +18,7 @@ const WeatherMap = () => {
   return (
     <div className="p-4">
       <iframe
-        src="https://www.rainviewer.com/map.html?loc=49.8876,16.2323,7&oFa=0&oC=0&oU=0&oCS=1&oF=0&oAP=1&c=1&o=83&lm=1&layer=radar&sm=1&sn=1&hu=0"
+        src="https://www.rainviewer.com/map.html?loc=49.8876,16.2323,7&oFa=0&oC=0&oU=0&oCS=1&oF=0&oAP=1&c=1&o=55&lm=1&layer=radar&sm=1&sn=1&hu=0"
         width="100%"
 
         className="h-[90vh] rounded-2xl"

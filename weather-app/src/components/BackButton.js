81868580000f7005a26c23573aa9fbb59a21cf19
Ejdy <adamhonzikcz@seznam.cz>
2024-03-12 17:43:49 +0100

    feat:added back button in status page

---
+++ weather-app/src/components/BackButton.js
@@ -0,0 +1,16 @@
+"use client"
+import {Button} from "@nextui-org/button";
+import {useRouter} from "next/navigation";
+import {ArrowLeft} from "lucide-react";
+const BackButton = ({className}) => {
+  const router = useRouter();
+  return (
+    <>
+      <Button className={className? className : "absolute m-16"} variant="light" isIconOnly onClick={()=>{router.back()}}>
+        <ArrowLeft size={24} color={"white"}/>
+      </Button>
+    </>
+  );
+};
+
+export default BackButton;

8e1ef69470c6bf16cd15b0428a609008293cbe5b
Ejdy <adamhonzikcz@seznam.cz>
2024-03-13 09:07:11 +0100

    fix: size of dropdown

--- weather-app/src/components/main/MainChartLoading.js
+++ weather-app/src/components/main/MainChartLoading.js
@@ -2,7 +2,7 @@ import {Spinner} from "@nextui-org/spinner";
 
 const MainChartLoading = () => {
   return (
-    <div className="w-full h-[300px] flex justify-center">
+    <div className="md:ml-[5vw] md:w-[90vw] w-[94vw] h-[300px] flex justify-center">
       <Spinner size="lg" />
     </div>
   );

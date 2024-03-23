d57be008f1698dbcb3541b2497c1024254e302f8
Ejdy <adamhonzikcz@seznam.cz>
2024-03-23 15:11:08 +0100

    fix: height of loading for chart

--- weather-app/src/components/main/MainChartLoading.js
+++ weather-app/src/components/main/MainChartLoading.js
@@ -2,7 +2,7 @@ import {Spinner} from "@nextui-org/spinner";
 
 const MainChartLoading = ({isForHistory}) => {
   return (
-    <div className={`${!isForHistory && "md:ml-[5vw]"} md:w-[90vw] w-[94vw] text-white flex justify-center`}>
+    <div className={`${!isForHistory && "md:ml-[5vw]"} md:w-[90vw] w-[94vw]  h-[356px]  text-white flex justify-center`}>
       <Spinner size="lg" />
     </div>
   );

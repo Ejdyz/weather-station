fc423f67a422b91faaf23f2aa25696af99da850e
Jan Adam <adamhonzikcz@seznam.cz>
2024-03-24 15:51:43 +0100

    feat: added more days to switcher

--- weather-app/src/components/DaySwitcher.js
+++ weather-app/src/components/DaySwitcher.js
@@ -1,18 +1,21 @@
 "use client"
 import {Select, SelectItem} from "@nextui-org/react";
 const DaySwitcher = ({value, setValue}) => {
-
   const handleSelectionChange = (e) => {
     setValue(e.target.value);
   };
   const days = [
+    {label: "1 Den", value: 1, },
+    {label: "2 Dny", value: 2, },
+    {label: "5 Dní", value: 5, },
     {label: "7 Dní", value: 7, },
     {label: "10 Dní", value: 10, },
     {label: "14 Dní", value: 14, },
     {label: "30 Dní", value: 30, },
     {label: "60 Dní", value: 60, },
     {label: "100 Dní", value: 100, },
-    ];
+  ];
+
   return (
     <div className="flex w-full md:max-w-xs flex-col px-2 ">
       <Select

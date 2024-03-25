956297cd5ade9d20e957700b315b2018e335a4a9
Ejdy <adamhonzikcz@seznam.cz>
2024-03-25 09:11:51 +0100

    feat: added description to moon phase

--- weather-app/src/components/MoonAndSun/moonphase/MoonPhase.js
+++ weather-app/src/components/MoonAndSun/moonphase/MoonPhase.js
@@ -12,48 +12,96 @@ import {
 
 const MoonPhase = async ({className}) => {
   const moonPhase = await getMoonPhase()
+
+  const translate = (moonPhase) => {
+    if (moonPhase === "New Moon") return "Nov";
+    if (moonPhase === "Full Moon") return "Úplněk";
+    if (moonPhase === "Waning Gibbous") return "Ubývající srpek";
+    if (moonPhase === "Waning Crescent") return "Ubývající měsíc";
+    if (moonPhase === "1st Quarter") return "První čtvrť";
+    if (moonPhase === "3rd Quarter") return "Třetí čtvrť";
+    if (moonPhase === "Waxing Gibbous") return "Přibývající srpek";
+    if (moonPhase === "Waxing Crescent") return "Přibývající měsíc";
+    return "Nov";
+  }
   if (moonPhase === "New Moon"){
     return (
-      <NewMoonIcon className={className} />
+      <div>
+
+        <NewMoonIcon className={className} />
+        <h3 className="text-white text-center">{translate(moonPhase)}</h3>
+      </div>
     );
   }
   if (moonPhase === "Full Moon"){
     return (
-      <FullMoonIcon className={className} />
+      <div>
+
+        <FullMoonIcon className={className} />
+        <h3 className="text-white text-center">{translate(moonPhase)}</h3>
+      </div>
     );
   }
   if (moonPhase === "Waning Gibbous"){
     return (
-      <MoonWaningGibbousIcon className={className} />
+      <div>
+
+        <MoonWaningGibbousIcon className={className} />
+        <h3 className="text-white text-center">{translate(moonPhase)}</h3>
+      </div>
     );
   }
   if (moonPhase === "Waning Crescent"){
     return (
-      <MoonWaningCrescentIcon className={className} />
+      <div>
+
+        <MoonWaningCrescentIcon className={className} />
+        <h3 className="text-white text-center">{translate(moonPhase)}</h3>
+      </div>
     );
   }
   if (moonPhase === "1st Quarter"){
     return (
-      <MoonFirstQuarterIcon className={className} />
+      <div>
+
+        <MoonFirstQuarterIcon className={className} />
+        <h3 className="text-white text-center">{translate(moonPhase)}</h3>
+      </div>
     );
   }
   if (moonPhase === "3rd Quarter"){
     return (
-      <MoonLastQuarterIcon className={className} />
+      <div>
+
+        <MoonLastQuarterIcon className={className} />
+        <h3 className="text-white text-center">{translate(moonPhase)}</h3>
+      </div>
     );
   }
   if (moonPhase === "Waxing Gibbous"){
     return (
-      <MoonWaxingGibbousIcon className={className} />
+      <div>
+
+        <MoonWaxingGibbousIcon className={className} />
+        <h3 className="text-white text-center">{translate(moonPhase)}</h3>
+      </div>
     );
   }
   if (moonPhase === "Waxing Crescent"){
     return (
-      <MoonWaxingCrescentIcon className={className} />
+      <div>
+
+        <MoonWaxingCrescentIcon className={className} />
+        <h3 className="text-white text-center">{translate(moonPhase)}</h3>
+      </div>
     );
   }
   return (
-    <NewMoonIcon className={className} />
+    <div>
+
+      <NewMoonIcon className={className} />
+      <h3 className="text-white text-center">{translate(moonPhase)}</h3>
+    </div>
   );
 };
 

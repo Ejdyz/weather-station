d95fcac92a26e8a70402b1cb458b620488e8978f
Ejdy <adamhonzikcz@seznam.cz>
2024-04-08 10:06:31 +0200

    fix: passing right data to isNight function

--- weather-app/src/app/page.js
+++ weather-app/src/app/page.js
@@ -72,7 +72,7 @@ export default async function Home() {
   })
   const cloudiness = await getHowCloudyCurrentlyIs(data)
   const rain = await getHowMuchIsCurrentlyRaining(data)
-  const night = await isNight(sunriseData.sunset, "09:55")
+  const night = await isNight(sunriseData.sunset, sunriseData.sunrise)
   return (
     <>
       <div className={await getWeatherBackground(cloudiness,rain,night)} >

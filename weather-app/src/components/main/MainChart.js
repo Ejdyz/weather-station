3d337bb570b07191abea494e9dd1c5c6504afb7c
Ejdy <adamhonzikcz@seznam.cz>
2024-04-21 21:17:35 +0200

    fix: removed one minute from time shown in graph

--- weather-app/src/components/main/MainChart.js
+++ weather-app/src/components/main/MainChart.js
@@ -12,7 +12,7 @@ const MainChart = ({data,isForHistory }) => {
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Month is zero-based
     const year = date.getFullYear();
     const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const minutes = String(date.getMinutes()-1).padStart(2, '0');
 
     return `${day}/${month}/${year} ${hours}:${minutes}`;
   }

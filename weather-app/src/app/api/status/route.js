5bb38bf6b5010d14f2a1e54dd93ebc34f2def021
Ejdy <adamhonzikcz@seznam.cz>
2024-05-06 10:23:02 +0200

    fix: no caching in status route response

--- weather-app/src/app/api/status/route.js
+++ weather-app/src/app/api/status/route.js
@@ -7,12 +7,12 @@ export async function GET() {
   try {
     const status = await getWeatherStationStatus();
     if(status.isActive){
-      return NextResponse.json({success:"active", data:status.data, currentTimeAtServer: new Date()}, { status: 200 })
+      return NextResponse.json({success:"active", data:status.data, currentTimeAtServer: new Date()}, { status: 200 , headers: {'Cache-Control': 'no-store, max-age=0'} })
     }else{
-      return NextResponse.json({error:"not active", data:status.data, currentTimeAtServer: new Date()}, { status: 500 })
+      return NextResponse.json({error:"not active", data:status.data, currentTimeAtServer: new Date()}, { status: 500 , headers: {'Cache-Control': 'no-store, max-age=0'} })
     }
   } catch (error) {
     console.error("Unable to connect to the database:", error.original);
-    return NextResponse.json({error:"server error"}, { status: 500 })
+    return NextResponse.json({error:"server error"}, { status: 500 , headers: {'Cache-Control': 'no-store, max-age=0'} })
   }
 }

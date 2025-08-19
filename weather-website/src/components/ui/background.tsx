5ac8121701d52476497e27c12d33794f1dd54ece
Jan Adam <adamhonzikcz@seznam.cz>
2025-08-19 14:26:42 +0200

    feat: added background component with gradient variables

---
+++ weather-website/src/components/ui/background.tsx
@@ -0,0 +1,51 @@
+"use client"
+import { useState } from "react";
+
+export default function Background({ children/*, type */} : { children: React.ReactNode /*, type: "overcast" | "clear" | "partly cloudy" | "cloudy" | "fog" | "rain" | "night" */}) {
+  const [type, setType] = useState("clear");
+
+  const availableTypes = [ "clear", "partly cloudy", "cloudy","overcast", "rain", "fog", "night"];
+  let backgroundClass = "";
+  switch(type) {
+    case "overcast":
+      backgroundClass = "bg-overcast";
+      break;
+    case "clear":
+      backgroundClass = "bg-clear";
+      break;
+    case "partly cloudy":
+      backgroundClass = "bg-partly-cloudy";
+      break;
+    case "cloudy":
+      backgroundClass = "bg-cloudy";
+      break;
+    case "fog":
+      backgroundClass = "bg-fog";
+      break;
+    case "rain":
+      backgroundClass = "bg-rain";
+      break;
+    case "night":
+      backgroundClass = "bg-night";
+      break;
+    default:
+      backgroundClass = "";
+  }
+
+  return (
+    <div className={`h-screen fixed top-0 overflow-auto w-full p-4 max-w-screen ${backgroundClass}`}>
+      <div className="flex gap-2">
+        {availableTypes.map((availableType) => (
+          <button
+            key={availableType}
+            onClick={() => setType(availableType)}
+            className={`px-4 py-2 rounded ${type === availableType ? "bg-gray-800 text-white" : "bg-gray-200 text-black"}`}
+          >
+            {availableType}
+          </button>
+        ))}
+      </div>
+      {children}
+    </div>
+  )
+}

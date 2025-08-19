d1853535dc91e23fb526ce9b1f16365c5ff5bb1f
Jan Adam <adamhonzikcz@seznam.cz>
2025-08-19 14:28:43 +0200

    feat: created base component for dividing data

--- weather-website/src/components/ui/base.tsx
+++ weather-website/src/components/ui/base.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function Base({ children } : { children: React.ReactNode }) {
+export default function Base({ children, className } : { children: React.ReactNode, className?: string }) {
   return (
-    <div className='rounded-md bg-white/30 bg-opacity-0 p-2'>
+    <div className={`rounded-md bg-white/30 bg-opacity-0 p-2 ${className}`}>
       {children}
     </div>
   )

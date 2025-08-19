6ed50195c578db6b1f317bcf38848ad09610b487
Jan Adam <adamhonzikcz@seznam.cz>
2025-08-19 17:16:38 +0200

    feat: moved the carousel to different file and added slider

---
+++ weather-website/src/components/ui/carousel.tsx
@@ -0,0 +1,58 @@
+"use client"
+import { JSX, useEffect, useState } from 'react';
+import { useKeenSlider } from "keen-slider/react";
+
+
+export function Carousel({ slides }:{slides: JSX.Element[]}) {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(0);
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    initial: 0,
+    loop: true,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(prev => prev + 1);
+    },
+  });
+
+  useEffect(() => {
+    setLoaded(prev => prev + 1);
+  }, []); 
+
+  if (!slides || slides.length === 0) {
+    return null;
+  }
+
+  if (loaded === 0) {
+    return <div className='animate-spin rounded-full h-5 w-5 border-b-2 border-white' />;
+  }
+
+  return (
+    <>
+      <div className="keen-slider h-full" ref={sliderRef}>
+        {slides.map((slide, index) => (
+          <div key={index} className="keen-slider__slide flex justify-center items-center">{slide}</div>
+        ))}
+      </div>
+      {loaded > 0 && instanceRef.current && (
+        <div className="flex justify-center gap-2">
+          {[
+            ...Array(instanceRef.current.track.details.slides.length).keys(),
+          ].map((idx) => {
+            return (
+              <button
+                key={idx}
+                onClick={() => {
+                  instanceRef.current?.moveToIdx(idx);
+                }}
+                className={"rounded-full border-1 border-white size-2 cursor-pointer" + (currentSlide === idx ? " bg-white " : "")}
+              ></button>
+            );
+          })}
+        </div>
+      )}
+    </>
+  )
+}
--- weather-website/src/components/ui/slider.tsx
+++ weather-website/src/components/ui/slider.tsx
@@ -1,58 +1,63 @@
 "use client"
-import { JSX, use, useEffect, useState } from 'react';
-import { useKeenSlider } from "keen-slider/react";
 
+import * as React from "react"
+import * as SliderPrimitive from "@radix-ui/react-slider"
 
-export function Slider({ slides }:{slides: JSX.Element[]}) {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [loaded, setLoaded] = useState(0);
-  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
-    initial: 0,
-    loop: true,
-    slideChanged(slider) {
-      setCurrentSlide(slider.track.details.rel);
-    },
-    created() {
-      setLoaded(prev => prev + 1);
-    },
-  });
+import { cn } from "@/lib/utils"
 
-  useEffect(() => {
-    setLoaded(prev => prev + 1);
-  }, []); 
-
-  if (!slides || slides.length === 0) {
-    return null;
-  }
-
-  if (loaded === 0) {
-    return <div className='animate-spin rounded-full h-5 w-5 border-b-2 border-white' />;
-  }
+function Slider({
+  className,
+  defaultValue,
+  value,
+  min = 0,
+  max = 100,
+  ...props
+}: React.ComponentProps<typeof SliderPrimitive.Root>) {
+  const _values = React.useMemo(
+    () =>
+      Array.isArray(value)
+        ? value
+        : Array.isArray(defaultValue)
+          ? defaultValue
+          : [min, max],
+    [value, defaultValue, min, max]
+  )
 
   return (
-    <>
-      <div className="keen-slider h-full" ref={sliderRef}>
-        {slides.map((slide, index) => (
-          <div key={index} className="keen-slider__slide flex justify-center items-center">{slide}</div>
-        ))}
-      </div>
-      {loaded > 0 && instanceRef.current && (
-        <div className="flex justify-center gap-2">
-          {[
-            ...Array(instanceRef.current.track.details.slides.length).keys(),
-          ].map((idx) => {
-            return (
-              <button
-                key={idx}
-                onClick={() => {
-                  instanceRef.current?.moveToIdx(idx);
-                }}
-                className={"rounded-full border-1 border-white size-2 cursor-pointer" + (currentSlide === idx ? " bg-white " : "")}
-              ></button>
-            );
-          })}
-        </div>
+    <SliderPrimitive.Root
+      data-slot="slider"
+      defaultValue={defaultValue}
+      value={value}
+      min={min}
+      max={max}
+      className={cn(
+        "relative flex w-full touch-none items-center select-none data-[disabled]:opacity-50 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col",
+        className
       )}
-    </>
+      {...props}
+    >
+      <SliderPrimitive.Track
+        data-slot="slider-track"
+        className={cn(
+          "bg-muted relative grow overflow-hidden rounded-full data-[orientation=horizontal]:h-1.5 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-1.5"
+        )}
+      >
+        <SliderPrimitive.Range
+          data-slot="slider-range"
+          className={cn(
+            "bg-primary absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full"
+          )}
+        />
+      </SliderPrimitive.Track>
+      {Array.from({ length: _values.length }, (_, index) => (
+        <SliderPrimitive.Thumb
+          data-slot="slider-thumb"
+          key={index}
+          className="border-primary bg-background ring-ring/50 block size-4 shrink-0 rounded-full border shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
+        />
+      ))}
+    </SliderPrimitive.Root>
   )
 }
+
+export { Slider }

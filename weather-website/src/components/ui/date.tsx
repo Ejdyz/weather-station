815ee9b4909869a435e54eaac386749e5fee4234
Jan Adam <adamhonzikcz@seznam.cz>
2025-08-23 14:45:33 +0200

    fix: updated date component

--- weather-website/src/components/ui/date.tsx
+++ weather-website/src/components/ui/date.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { formatDateForDisplay } from "@/lib/utils";
+import { formatDateToDisplayNumericFull } from "@/lib/utils";
 
 import React, { Suspense } from 'react'
 
 export default function DateComponent({ date }: { date: Date | string | undefined }) {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      {formatDateForDisplay(date)}
+      {formatDateToDisplayNumericFull(date)}
     </Suspense>
   );
 }

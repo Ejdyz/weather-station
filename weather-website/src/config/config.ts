22b6b0dce7feaf724596e38f2f549282e0556ade
Jan Adam <adamhonzikcz@seznam.cz>
2025-08-23 14:45:42 +0200

    feat: added default values to config

---
+++ weather-website/src/config/config.ts
@@ -0,0 +1,6 @@
+export const FALLBACK_SUNRISE = "06:00";
+export const FALLBACK_SUNSET = "18:00";
+export const FALLBACK_MOONRISE = "22:00";
+export const FALLBACK_MOONSET = "04:00";
+export const FALLBACK_GOLDEN_HOUR_START = "-:-";
+export const FALLBACK_GOLDEN_HOUR_END = "-:-";

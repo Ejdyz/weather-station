85ee503d7172fa7686ac25a7d78b435b88b19a56
Jan Adam <adamhonzikcz@seznam.cz>
2025-08-26 22:57:22 +0200

    fix: displaying correct date in recent history

--- weather-website/src/components/ui/date.tsx
+++ weather-website/src/components/ui/date.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { formatDateToDisplayNumericFull } from "@/lib/utils";
-
+import { formatWeatherData } from "@/lib/utils";
 import React, { Suspense } from 'react'
+import { Skeleton } from "./skeleton";
 
-export default function DateComponent({ date }: { date: Date | string | undefined }) {
+export default function DateComponent({ date, type }: { date: Date | undefined, type: "date_full_numeric" | "date_short_numeric" | "date_short_written" | "time_short_numeric" }) {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      {formatDateToDisplayNumericFull(date)}
+    <Suspense fallback={<Skeleton />}>
+      {formatWeatherData(type, date)}
     </Suspense>
   );
 }
